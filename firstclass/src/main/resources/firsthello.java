53992eabbfa7a9e99c513a984202c785db38749e
abhijit8401 <37523448+abhijit8401@users.noreply.github.com>
2018-03-19 20:50:38 -0400

    capitalism

--- firstclass/src/main/resources/firsthello.java
+++ firstclass/src/main/resources/firsthello.java
@@ -4,7 +4,8 @@ public class firsthello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is communism");
+		System.out.println("This is capitalisam");
+		
 	
 
 	}
